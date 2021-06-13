2267ee3b94ab07a03fce70be068d74ab01918d5e
Josh Junon <josh.junon@protonmail.com>
2021-06-14 00:12:05 +0200

    add patch for faulty generated javascript

---
+++ worker-patch.js
@@ -0,0 +1,5 @@
+// Patches for the absolutely broken GWT output...
+// Not sure why but this code is written for non-strict ES5 environments.
+// Completely unacceptable and nobody seems to want to fix this.
+
+var _xblockexpression;
